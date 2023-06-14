f31251d067362dd26f05a8c3c67121697bebe664
HyungCheolSim <sim34122@gmail.com>
2023-06-14 19:59:28 +0900

    first commit

---
+++ src/main/java/com/sparta/voyageblog/VoyageBlogApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.voyageblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoyageBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VoyageBlogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/voyageblog/VoyageBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.voyageblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoyageBlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
