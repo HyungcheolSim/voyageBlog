2c3f5c396c36294bc17a8e3fbc1cb8ff69ebdd64
HyungCheolSim <sim34122@gmail.com>
2023-06-16 15:53:21 +0900

    add comment

--- src/main/java/com/sparta/voyageblog/service/PostService.java
+++ src/main/java/com/sparta/voyageblog/service/PostService.java
@@ -24,26 +24,28 @@ public class PostService {
         return postRepository.findAllByOrderByCreatedAtDesc().stream().map(PostResponseDto::new).toList();
     }
 
+    //Post 생성
     public PostResponseDto createPost(PostRequestDto requestDto) {
         Post post= new Post(requestDto);
         Post savePost= postRepository.save(post);
         return new PostResponseDto(savePost);
     }
-
-
+    
+    //특정 id의 post 조회
     public PostResponseDto getPostById(Long id) {
          Post findPost=findPost(id);
          return new PostResponseDto(findPost);
     }
 
+    //Post 수정
     @Transactional
     public PostResponseDto updatePost(PostUpdateRequestDto requestDto) {
-        Post post=findPost(requestDto.getId());
+        Post post=findPost(requestDto.getId()); //존재찾기
 
         if(checkPassword(post.getPassword(),requestDto.getPassword())){//비밀번호 확인되면
-            post.update(requestDto); //dirty check로 update된다.
+            post.update(requestDto); //dirty check 로 update 된다.
         }
-        return new PostResponseDto(findPost(requestDto.getId()));
+        return new PostResponseDto(findPost(requestDto.getId())); //다시 불러와서 조회
     }
     @Transactional
     public String deletePost(Long id) {
@@ -52,13 +54,13 @@ public class PostService {
         return "true";
     }
 
-
-
-    //private methods
+    //private methods Service 내에서만 사용되는 메서드!
+    //비밀번호 일치 여부 확인 Boolean
     private Boolean checkPassword(String input,String password) {
         return Objects.equals(input, password);
     }
 
+    //특정 id를 가지는 Post 를 찾는 method, getPostById와 다른점은 리턴값이 post 객체라는 점
     private Post findPost(Long id){
         return postRepository.findById(id).orElseThrow(() ->
                 new IllegalArgumentException("선택한 게시글은 존재하지 않습니다.")
